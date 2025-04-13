dadc64a21bad65a3719cb1bec711ea3a36d960a6
airwolff <17932067+airwolff@users.noreply.github.com>
2025-04-12 20:02:37 -0400

    navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,16 +1,27 @@
+import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <>
-      <nav>
-        <a href=''>
-          <Image src='../../assets/logo.png' alt='logo' />
+      <nav className='w-full fixed px-5 lg:px-8 x1:px-[8%] py-4 flex items-center justify-between z-50'>
+        <a href='#top'>
+          <Image src={assets.logo} className="w-28 curser-pointer mr-14" alt='logo' />
         </a>
+        <ul className='hidden md:flex items-center'>
+          <li><a href="#top">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div>
+          <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3'alt='arrow'/></a>
+        </div>
       </nav> 
     </>
   )
 }
 
-export default navbar
+export default Navbar
