6cc640d65f39d9a8b5d40bbbb836e5ed5b259a18
airwolff <17932067+airwolff@users.noreply.github.com>
2025-04-18 15:47:48 -0400

    feat(Header): Implement component and resolve build issues
    - Develop Header component with profile image, name, title, contact link, and resume download link (using 'download' attribute).
    - Style component using Tailwind CSS.
    - Enforce PascalCase convention (`Header.tsx`, `Header` export).
    
    - Troubleshoot and fix persistent ts(1261) file casing error by clearing build caches (.next, node_modules) and restarting TS server.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,14 +2,32 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const header = () => {
+const Header = () => {
   return (
     <div>
       <div>
-        <Image src={assets.profile_img} alt="profile image"/>
+        <Image src={assets.profile_img} alt="profile image" className='rounded-full w-32'/>
+      </div>
+      <h3 className='flex items-end gap-2 text-xl md:text-2x1 mb-3 font-Ovo'>
+        Hi! I'm Andy Wolff 
+        <Image src={assets.hand_icon} alt='' className='W-6'/>
+      </h3>
+      <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+        Web Developer based in Mid-Coast Maine.
+      </h1>
+      {/* <p className='max-w-2x1 mx-auto font-Ovo'></p> */}
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+          contact me 
+          <Image src={assets.right_arrow_white} alt='arrow icon' className='w-4'/>
+        </a>
+        <a href="/resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+          my resume
+          <Image src={assets.download_icon} alt='arrow icon' className='w-4'/>
+        </a>
       </div>
     </div>
   )
 }
 
-export default header
+export default Header
